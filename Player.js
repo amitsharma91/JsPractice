36470445fcb98500c5245003c8d9036d7432e53d
Amit Sharma <amit@connectingdotsinfotech.com>
2018-11-20 17:33:53 +0530

    Added Prototype pattern in Player.js

--- Player.js
+++ Player.js
@@ -6,7 +6,19 @@ function Player(name , email){
     this.currentPoint = 0;
 }
 
-//create a prototype and constructor pattern
-Player.prototype() = {
-    
+//create a prototype pattern
+Player.prototype = {
+    constructor: Player,
+    updatePoints: function(newPointToUpdate){
+        this.pointsList.push(newPointToUpdate);
+    },
+    showPoints: function(){
+        var playerPoints = this.pointsList.length > 0 ? this.pointsList.join(",") : "no points scored yet";
+
+        return this.name + " points are: " + playerPoints;
+    },
+    updateEmail: function(emailToUpdate){
+        this.email = emailToUpdate;
+        return "email updated with " + emailToUpdate; 
+    }
 }
