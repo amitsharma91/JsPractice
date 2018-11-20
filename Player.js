b0a4d9e4d43dbaf20836722a62286c1993adac80
Amit Sharma <amit@connectingdotsinfotech.com>
2018-11-20 17:23:15 +0530

    added Player.js with Player Constructor

---
+++ Player.js
@@ -0,0 +1,12 @@
+//create constructor
+function Player(name , email){
+    this.name = name;
+    this.email = email;
+    this.pointsList = [];//list of points
+    this.currentPoint = 0;
+}
+
+//create a prototype and constructor pattern
+Player.prototype() = {
+    
+}
