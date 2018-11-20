6482d34a1ff0985c7fc9047ddca7d0e683d8ca40
Amit Sharma <amit@connectingdotsinfotech.com>
2018-11-20 17:44:11 +0530

    initialized & used Player Object

--- Player.js
+++ Player.js
@@ -15,10 +15,27 @@ Player.prototype = {
     showPoints: function(){
         var playerPoints = this.pointsList.length > 0 ? this.pointsList.join(",") : "no points scored yet";
 
-        return this.name + " points are: " + playerPoints;
+        return this.name + "'s points are: " + playerPoints;
     },
     updateEmail: function(emailToUpdate){
         this.email = emailToUpdate;
         return "email updated with " + emailToUpdate; 
     }
 }
+
+// instantiating and using object
+player1 = new Player("John","johndoe@gamil.com");
+
+// show points
+console.log(player1.showPoints());
+
+// add points
+player1.updatePoints(8);
+player1.updatePoints(9);
+
+//show points
+console.log(player1.showPoints());
+
+console.log(player1.email);
+player1.updateEmail("johndoe@gmail.co.in");
+console.log(player1.email);
