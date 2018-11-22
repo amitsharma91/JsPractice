83d4dca35655df98fd62273d502e53036b888d62
Amit Sharma <amit@connectingdotsinfotech.com>
2018-11-22 17:46:08 +0530

    added  prototype in Post class

--- PostExercise.js
+++ PostExercise.js
@@ -7,6 +7,28 @@ function Post(title,body,author){
     this.isLive = false;
 }
 
+Post.prototype = {
+    constructor : Post,
+    addComment : function(newComment){
+        this.comments.push(newComment);
+    },
+    getComments : function(){
+        return this.comments;
+    },
+    updateViewCount : function(){
+        this.viewCount ++;
+    }
+}
 
 var post1 = new Post("abc","askdjl asjldk","tomm");
 console.log(post1);
+
+console.log(post1.getComments());
+post1.addComment("hey, this post.....")
+console.log(post1.getComments());
+
+console.log("view count: "+ post1.viewCount);
+post1.updateViewCount();
+console.log("view count: "+ post1.viewCount);
+post1.updateViewCount();post1.updateViewCount();post1.updateViewCount();post1.updateViewCount();
+console.log("view count: "+ post1.viewCount);
